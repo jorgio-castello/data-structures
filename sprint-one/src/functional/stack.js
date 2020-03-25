84f1d5d6854ad39eae3d3c18768409177680c531
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 11:38:37 -0700

    someInstance.size() - uses Object.keys(storage) length to report the number of keys in   storage at any given time

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,9 +1,7 @@
 var Stack = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
-
   // Implement the methods below
   someInstance.push = function(value) {
   };
@@ -12,7 +10,8 @@ var Stack = function() {
   };
 
   someInstance.size = function() {
+    console.log( "size is running");
+    return Object.keys(storage).length;
   };
-
   return someInstance;
 };
