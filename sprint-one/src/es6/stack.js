ca7b0f60d4ff4db3b01fc41274e9c55801e0b84a
Jorge Castello <jorgio.castello@gmail.com>
2020-03-25 14:53:31 -0700

    Add stack logic (ES6)
    Steps:
      1. Within the constructor function of the Stack Class - declare a
      property on this and assign it the value of an empty object
      2. Declare methods within the Stack Class Invocation
      3. These methods include push, pop, and size

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,18 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
-
+  push(value) {
+    let key = this.size();
+    this.storage[key] = value;
+  }
+  pop() {
+    let popped = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return popped;
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
