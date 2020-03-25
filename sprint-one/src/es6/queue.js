4d175c506eeae8b90eb69d5375b9b581a785be6a
Jorge Castello <jorgio.castello@gmail.com>
2020-03-25 14:58:32 -0700

    Add queue logic (ES6)
    Steps:
      1. Within the constructor function of the Class Queue Invocation,
      Declare a property on this called storage and assign it the value
      of an empty object
      2. Create methods within the Class Queue Invocation
      3. These methods include enqueue, dequeue, and size

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,22 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    let keys = Object.keys(this.storage);
+    for (let i = 0; i < keys.length; i++) {
+      this.storage[i + 1] = this.storage[i];
+    }
+    this.storage[0] = value;
+  }
+  dequeue() {
+    let popped = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return popped;
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
