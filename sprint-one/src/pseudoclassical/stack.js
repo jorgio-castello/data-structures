53dfd0bed0d1c9b30662153eb7e6c3107c8c9718
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 13:28:06 -0700

    returns 0 when we have a new, empty stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,13 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	this.storage = {};
 };
 
+Stack.prototype = {
+	'push' : function(){},
+	'pop': function(){},
+	'size': function(){
+		return Object.keys( this.storage ).length;
+	}
+};
 
+var myStack = new Stack;
