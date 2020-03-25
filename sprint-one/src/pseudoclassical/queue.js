f15d66b0d2f0a0c1b240ba9f03498b937a1c8132
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 15:52:25 -0700

    Pseudoclassical queue done

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -4,8 +4,21 @@ var Queue = function() {
 
 Queue.prototype = {
 	'constructor': Queue,
-	'enqueue': function( value ) {},
-	'dequeue': function() {},
+	'enqueue': function( value ) {
+		let iMax = this.size() -1;
+		if ( iMax !== -1 ) {
+			for( var i = iMax; i >= 0; i-- ) {
+				this.storage[i+1] = this.storage[i];
+			}
+		}
+		this.storage[0] = value;
+	},
+	'dequeue': function() {
+		let i = this.size() -1;
+		let dequeuedValue = this.storage[i];
+		delete this.storage[i];
+		return dequeuedValue;
+	},
 	'size': function(){
 		return Object.keys(this.storage).length;
 	}
