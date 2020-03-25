c4575aa8703f0060d7395f8cdbc5b76ce6747391
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 15:47:39 -0700

    Usage of Queue.prototype to make a constructor function -- well, a constructor key that points to the Queue var that is itself a function that makes an object??
    Then also putting the methods in so we can use Queue.prototype.dothething()

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,13 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Queue.prototype = {
+	'constructor': Queue,
+	'enqueue': function( value ) {},
+	'dequeue': function() {},
+	'size': function(){
+		return Object.keys(this.storage).length;
+	}
+}
 
