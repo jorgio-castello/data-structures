f73a9359a31f7f57ca5dff47c3d2c8e4688e2bbb
Jorge Castello <jorgio.castello@gmail.com>
2020-03-25 11:43:10 -0700

    Add logic for stack (functional instantiation)
    Steps:
      1. Declare a property storage on the someInstance object
      2. For push method:
        2a. Declare a value key and assign it the result of invoking
        someInstance.size() - if the storage obj is empty, the first key
        will be 0, the second key will be 1, etc.
        2b. Declare a property on the storage object with the key created
        above, and the value that is passed as a parameter to the method
      3. For pop method:
        3a. Declare a variable popped, and assign it to the the value
        of the most recently added key
        3b. Delete the most recently added key
        3c. Return popped
      4. For size method:
        4a. Return the number of keys in the storage object
      5. Return the someInstance object, which includes a storage object,
      and three methods: push, pop, and size

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,22 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    let key = someInstance.size();
+    someInstance.storage[key] = value;
   };
 
   someInstance.pop = function() {
+    let popped = someInstance.storage[someInstance.size() - 1];
+    delete someInstance.storage[someInstance.size() - 1];
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(someInstance.storage).length;
   };
 
   return someInstance;
