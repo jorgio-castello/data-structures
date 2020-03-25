9ea16764d90d6774a6d83d3a6c0821ccaddf32e5
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 15:01:46 -0700

    implemented the methods

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,16 +1,23 @@
 class Stack {
   constructor() {
+  	this.storage = {}
   }
 
   push( value ) {
-
+  	var nextKey = this.size();
+  	this.storage[nextKey] = value;
   }
 
-  pop() {
-
+  pop( value ) {
+  	var keyToPop = this.size() - 1;
+  	if( keyToPop > -1 ) {
+  		let poppedValue = this.storage[keyToPop];
+  		delete this.storage[keyToPop];
+  		return poppedValue;
+  	}
   }
 
   size() {
-  	
+  	return Object.keys(this.storage).length;
   }
 }
