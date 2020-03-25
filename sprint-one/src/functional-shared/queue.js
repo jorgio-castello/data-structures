9ce076fed831431a216a98238c0ca19c14fb4665
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 15:33:23 -0700

    adding items

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,14 +1,26 @@
 var Queue = function() {
-	storage = {}
-	Object.assign( storage, queueMethods );
-	return storage;
+	queue = {
+		storage: {}
+	}
+	Object.assign( queue, queueMethods );
+	return queue;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var queueMethods = {
-  'size': function(){},
-  'enqueue': function(){},
+  'size': function(){
+  	return Object.keys(this.storage).length;
+  },
+  'enqueue': function( value ) {
+  	var lastIndex = this.size() -1;
+  	if( lastIndex > -1 ) {
+  		for( var i = lastIndex; i >= 0; i-- ) {
+          this.storage[i+1] = this.storage[i];
+  		}
+  	}
+  	this.storage[0] = value;
+  },
   'dequeue': function(){}	
 };
 
