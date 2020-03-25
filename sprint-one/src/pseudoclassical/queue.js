77406129be71852e69f11eb821aab48a7014065f
Jorge Castello <jorgio.castello@gmail.com>
2020-03-25 14:49:25 -0700

    Add queue logic (pseudoclassical)
    Steps:
      1. Declare a property on this called storage and assign it the value
      of an empty object
      2. Define prototype methods on the Queue instantiation object
      3. These methods include enqueue, dequeue, and size

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  let keys = Object.keys(this.storage);
+  for (let i = 0; i < keys.length; i++) {
+    this.storage[i + 1] = this.storage[i];
+  }
+  this.storage[0] = value;
+};
+Queue.prototype.dequeue = function() {
+  let popped = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return popped;
+};
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
 
