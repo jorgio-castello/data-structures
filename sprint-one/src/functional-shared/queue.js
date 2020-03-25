3b65c28a79aa7348284ee15b3a0c8fd20097c4f9
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 15:37:37 -0700

    functional shared complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,19 +1,19 @@
 var Queue = function() {
-	queue = {
-		storage: {}
-	}
+	var queue = {};
+	queue.storage = {};
 	Object.assign( queue, queueMethods );
 	return queue;
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-};
+ };
 
 var queueMethods = {
   'size': function(){
   	return Object.keys(this.storage).length;
   },
+  'getLastIndex' : function () {
+  	return this.size() - 1;
+  },
   'enqueue': function( value ) {
-  	var lastIndex = this.size() -1;
+  	var lastIndex = this.getLastIndex();
   	if( lastIndex > -1 ) {
   		for( var i = lastIndex; i >= 0; i-- ) {
           this.storage[i+1] = this.storage[i];
@@ -21,7 +21,12 @@ var queueMethods = {
   	}
   	this.storage[0] = value;
   },
-  'dequeue': function(){}	
+  'dequeue': function(){
+  	var lastIndex = this.getLastIndex();
+  	var dequeuedValue = this.storage[lastIndex];
+  	delete this.storage[lastIndex];
+  	return dequeuedValue;
+  }	
 };
 
 
