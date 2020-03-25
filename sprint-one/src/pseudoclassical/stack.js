e087c15120bf42ccfddc9c50061275606abb8551
Jorge Castello <jorgio.castello@gmail.com>
2020-03-25 14:43:36 -0700

    Add stack logic (pseudoclassical)
    Steps:
    1. Create a property on this called storage and assign it an empty obj
    2. Declare prototype methods on the Stack instantiation object
    3. These methods include push, pop, and size

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+Stack.prototype.push = function(value) {
+  let key = this.size();
+  this.storage[key] = value;
+};
+Stack.prototype.pop = function() {
+  let popped = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return popped;
+};
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
 
 
