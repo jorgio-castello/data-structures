45ea8f3c17b344334fa53c480e57c51efaff649f
Jorge Castello <jorgio.castello@gmail.com>
2020-03-25 13:21:14 -0700

    Add queue logic (prototypal pattern)
    Steps:
      1. Declare instance and assign it Object.create(queueMethods)
      2. Declare a storage property on instance and set it to an empty obj
      3. Return instance
    
      Note: queueMethods include enqueue, dequeue, and size

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let instance = Object.create(queueMethods);
+  instance.storage = {};
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    let keys = Object.keys(this.storage);
+    for (let i = keys.length - 1; i >= 0; i--) {
+      this.storage[i + 1] = this.storage[i];
+    }
+    this.storage[0] = value;
+  },
+  dequeue: function() {
+    let popped = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return popped;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
