943d72fe914d3465a6b79a5aa481ece696a3b952
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 12:02:36 -0700

    someInstance.push rewritten

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,8 +2,9 @@ var Stack = function() {
 	var someInstance = {};
 	var storage = {};
 
-	someInstance.push = function () {
-		//do stuff
+	someInstance.push = function ( value ) {
+		var nextKey = someInstance.size();
+		storage[nextKey] = value;
 	}
 
 	someInstance.pop = function () {
@@ -12,7 +13,6 @@ var Stack = function() {
 
 	someInstance.size = function () {
 		return Object.keys(storage).length;
-		//do stuff
 	}
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
