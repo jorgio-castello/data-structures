45be43e3c0d4f25ca6cbe2c28dd7c43d17ef973d
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 11:59:01 -0700

    declaring the needed pop/push/size functions, returning someInstance

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
+	var someInstance = {};
+	var storage = {};
+
+	var someInstance.push = function () {
+		//do stuff
+	}
+
+	var someInstance.pop = function () {
+		//do stuff
+	} 
+
+	var someInstance.size = function () {
+		//do stuff
+	}
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return someInstance;
 };
 
 var stackMethods = {};
