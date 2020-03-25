3e1b69dcdac4ed0ea17d0c1f53057d3dbb8e1a41
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 15:15:14 -0700

    enqueue implemented

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,6 +7,14 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var initialSize = someInstance.size();
+    if( initialSize > 0 ){ 
+      for( var i = someInstance.size(); i >= 0; i-- ) {
+        storage[i] = storage[i -1]
+      }
+    }
+    storage[0] = value;
+    console.log( storage );
   };
 
   someInstance.dequeue = function() {
