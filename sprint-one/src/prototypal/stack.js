04e937bd4d1bdcf0bdb60578b58d79809b109f15
Jorge Castello <jorgio.castello@gmail.com>
2020-03-25 13:15:16 -0700

    Add stack logic (prototypal pattern)
    Steps
      1. Declare instance and assign it Object.create(stackMethods)
      2. Create a property on instance called storage and assign it an
      empty object
      3. Return instance
    
      Note: stackMethods include push, pop, and size

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let instance = Object.create(stackMethods);
+  instance.storage = {};
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    let key = this.size();
+    this.storage[key] = value;
+  },
+  pop: function() {
+    let popped = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return popped;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
