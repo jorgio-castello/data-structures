338c8a3b23421569d09d3b354bf43a9e9634f223
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 11:56:29 -0700

    Refactoring for legibility

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,36 +1,23 @@
 var Stack = function() {
   var someInstance = {};
-  // Use an object with numeric keys to store values
   var storage = {};
-  // Implement the methods below
+ 
   someInstance.push = function(value) {
-    console.log( "Push running!" );
-    console.log( storage );
     let keyName = someInstance.size();
     storage[keyName] = value;
-    console.log( "Push done!" );
-    console.log( storage );
   };
 
   someInstance.pop = function() {
-    console.log( "Pop initialized" );
-    console.log( storage );
-    let keyName = someInstance.size() -1;
-    if( keyName < 0 ) {
-      console.log( "nothing to pop!" );
-      return;
+    let keyToPop = someInstance.size() - 1;
+    if( keyToPop >= 0 ){
+      var poppedValue = storage[keyToPop];
+      delete storage[keyToPop];
+      return poppedValue;
     }
-    console.log( "Pop running, we gotta delete " + keyName + " which holds " + storage[keyName]);
-    let returnValue = storage[keyName];
-    delete storage[keyName];
-
-    console.log( "Pop done~ We have popped off:" + returnValue );
-    console.log( storage );
-    return returnValue;
+    return;
   };
 
   someInstance.size = function() {
-    console.log( "size is running!" );
     return Object.keys(storage).length;
   };
   return someInstance;
