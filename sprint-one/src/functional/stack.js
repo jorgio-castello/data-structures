44e0c34b7db60d20a2f5524cc1db37231766954d
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 11:44:10 -0700

    completion of someInstance.push

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,13 +4,16 @@ var Stack = function() {
   var storage = {};
   // Implement the methods below
   someInstance.push = function(value) {
+    let keyName = someInstance.size();
+    console.log( "Key:" + keyName );
+    storage[keyName] = value;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    console.log( "size is running");
+    console.log( "size is running" );
     return Object.keys(storage).length;
   };
   return someInstance;
