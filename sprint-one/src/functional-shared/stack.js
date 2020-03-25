a6e7709ce0e09e81d388fc775e9c1748c57b03ff
Jorge Castello <jorgio.castello@gmail.com>
2020-03-25 12:54:22 -0700

    Add stack logic (functional-shared instantiation)
    1. Declare variable instance which contains an empty object called
    storage
    2. Declare a stackMethods object, which contains the following
      2a. push method (as formerly described in functional instantiation)
      2b. pop method (as formerly described in functional instantiation)
      2c. size method (as formerly described in functional instantiation)
    3. extend helper function
      3a. extend adds the properties found in stackMethods to the instance
      object upon invocating extend
    4. After the methods in stackMethods have been added to instance,
    instance is returned

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let instance = {
+    storage: {},
+  };
+  extend(instance, stackMethods);
+
+  return instance;
 };
 
-var stackMethods = {};
+let stackMethods = {
+  push: function(value) {
+    let key = this.size();
+    this.storage[key] = value;
+  },
+  pop: function() {
+    let popped = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return popped;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
+let extend = (obj, methods) => {
+  for (let method in methods) {
+    obj[method] = methods[method];
+  }
+  return obj;
+};
 
