f1dc30e80dba125fb0d4c178a1c7a3e94d8a4157
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 15:27:17 -0700

    Functional-shared going on here
    - queue methods have 'size', 'enqueue' and 'dequeue' keys which
      hold functions
    
    - in Queue we're using Object.assign to extend  the storage object
      with what's in the queueMethods object

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,15 @@
 var Queue = function() {
+	storage = {}
+	Object.assign( storage, queueMethods );
+	return storage;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueMethods = {
+  'size': function(){},
+  'enqueue': function(){},
+  'dequeue': function(){}	
+};
 
 
