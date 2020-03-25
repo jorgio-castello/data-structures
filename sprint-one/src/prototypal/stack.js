aa2f76ff362210e07f2de09ccc49c2575d042a3c
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 13:16:11 -0700

    Size and push added to stackmethods

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,21 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+	var someInstance = Object.create(stackMethods); //This will allow it to use a prototypal chain 
+	someInstance.storage = {}
+	return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	'push': function( value ) {
+		let nextKey = this.size();
+		this.storage[nextKey] = value;
+	//do things
+	},
+	'pop': function() {
+	//do things
+	},
+	'size': function(){ 
+		return Object.keys(this.storage).length;
+	},
+};
 
 
