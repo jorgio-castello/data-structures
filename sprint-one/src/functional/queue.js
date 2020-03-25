aa6117840b51b4f25e8333f9b687700d3ad3d19f
Jorge Castello <jorgio.castello@gmail.com>
2020-03-25 11:59:03 -0700

    Add logic for queue (function instantiation)
    Steps:
      1. Add storage as a property of someInstance
      2. enqeueu method:
        2a. Store the keys of storage as an array
        2b. Loop through the keys
        2c. Re-assign the current value of the property from its current
        index to index + 1
        2d. After the for-loop, assign storage[0] equal to the value that
        was passed as a parameter to this method invocation
      3. dequeue method:
        3a. Declare a variable popped, and assign it the value of the
        property that was added first
        3b. Delete that property from the storage object
        3c. Return popped
      4. size method:
        4a. Return the length / number of keys
      5. Return someInstance - which includes the storage object, and the
      queue, dequeue, and size methods

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,26 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    let keys = Object.keys(someInstance.storage);
+    for (let i = keys.length - 1; i >= 0; i--) {
+      someInstance.storage[i + 1] = someInstance.storage[i];
+    }
+    someInstance.storage[0] = value;
   };
 
   someInstance.dequeue = function() {
+    let popped = someInstance.storage[someInstance.size() - 1];
+    delete someInstance.storage[someInstance.size() - 1];
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(someInstance.storage).length;
   };
 
   return someInstance;
