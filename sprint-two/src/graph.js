ba62b8946b19e9bf9b69e90138bab7b93adfae0c
Jorge Castello <jorgio.castello@gmail.com>
2020-03-26 15:57:47 -0700

    Add logic for hasEdge and addEdge graph methods
    hasEdge determines if a given node has an edge with another node
    addEdge will add an edge from a first node to a second node

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -9,21 +9,21 @@ var Graph = function() {
 var graphNode = function(value, id) {
   this.id = id;
   this.value = value;
-  this.PointsTo = [];
-}
+  this.pointsTo = [];
+};
 
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  let newNode = new graphNode( node,  this.nextID );
+  let newNode = new graphNode( node, this.nextID );
   this.nodes.push(newNode);
   this.nextID++;
- };
+};
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  for(let i = 0; i < this.nodes.length; i++) {
-    if(this.nodes[i].value === node) {
+  for (let i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i].value === node) {
       return true;
     }
   }
@@ -31,20 +31,20 @@ Graph.prototype.contains = function(node) {
 };
 
 Graph.prototype.findNode = function(node) {
-  for( let i = 0; i < this.nodes.length; i++ ) {
-    if( this.nodes[i].value == node ) {
+  for ( let i = 0; i < this.nodes.length; i++ ) {
+    if ( this.nodes[i].value === node ) {
       return this.nodes[i].id;
     }
   }
   return -1;
-}
+};
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
   // TODO: do we want to cascade through what points to this?
   var nodeId = this.findNode(node);
-  for( var i = 0; i < this.nodes.length; i++ ) {
-    if( this.nodes[i].id === nodeId ) {
+  for ( var i = 0; i < this.nodes.length; i++ ) {
+    if ( this.nodes[i].id === nodeId ) {
       this.nodes.splice(i, 1);
     }
   }
@@ -52,10 +52,30 @@ Graph.prototype.removeNode = function(node) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var fromNodeId = this.findNode( fromNode );
+  var toNodeId = this.findNode( toNode );
+
+  for ( var i = 0; i < this.nodes.length; i++ ) {
+    if ( this.nodes[i].id === fromNodeId ) {
+      return this.nodes[i].pointsTo.includes( toNodeId );
+    }
+  }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  // add the id of the "toNode" to the pointsTo key of the "fromNode"
+  let fromNodeId = this.findNode(fromNode);
+  let toNodeId = this.findNode(toNode);
+
+  //Iterate over the nodes - when we find the node with fromNodeId
+  for (let i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i].id === fromNodeId) {
+      //push toNodeId into the found node's pointsTo array
+      this.nodes[i].pointsTo.push(toNodeId);
+    }
+  }
+
 };
 
 // Remove an edge between any two specified (by value) nodes.
