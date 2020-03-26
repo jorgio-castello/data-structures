49befe38665e9bc9f0c53cefb2ce9c078f45e972
Marissa Conner <marissafromtexas@gmail.com>
2020-03-26 10:40:36 -0700

    revision of contains, now properly navigates the tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -8,49 +8,38 @@ var Tree = function(value) {
   return newTree;
 };
 
+/*
+var Node = function(value){
+  var newNode = {};
+  newNode.value = value;
+  newNode.children = [];
+  return newNode;
+}
+*/
+
+// We are creating an object with a children array:
+// That children array stores new invocatins of an additional Tree object
+// Every node can have a value but also children.
+// So we don't have to define a root, because there's only one Tree object
+// Any invocations of a new Tree is stored within the children key of the original Tree at some nested point
+
 var treeMethods = {};
+
 treeMethods.addChild = function(value) {
-
-  // if newTree == {}
-  //then add a key called root,
-  if(this.value === undefined) {
-    this.root = this.children;
-  }
-  /*
-  {
-    'root': {
-      value: 1234,
-      children: []
-    }
-  }
-
-  */
-
   let newChild = Tree(value);
   this.children.push(newChild);
 };
 
-//let newTree = Tree();
-//newTree.addChild
-//
-
-// var Node = function(value) {
-//   var node = {};
-//   node.value = value;
-//   node.children = [];
-//   return node;
-// };
-
-
 treeMethods.contains = function(target) {
-  let foundTarget;
   var traverseTree = function( node ) {
     if( node.value === target ) {
       return true;
     } else {
       if ( node.children ) {
         for(let i = 0; i < node.children.length; i++) {
-          return traverseTree( node.children[i] )
+          if(traverseTree( node.children[i] )) {
+            return true;
+          }
         }
       }
     }
