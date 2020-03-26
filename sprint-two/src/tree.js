cbc4e535c12098dfeac4b486d69b2e3d69e92639
Marissa Conner <marissafromtexas@gmail.com>
2020-03-25 22:39:09 -0700

    extends newTree with treeMethods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend( newTree, treeMethods );
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
   return newTree;
 };
 
